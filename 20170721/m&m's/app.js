fb0b5af9ffb11a5fe7f2e98983fd5582ce64b560
Seiei Miyagi <hanachin@gmail.com>
2017-07-22 12:50:09 +0900

    Remove CE button, add tax button

--- 20170721/m&m's/app.js
+++ 20170721/m&m's/app.js
@@ -3,6 +3,11 @@ $(function() {
     $("button").click(function() {
         if ($(this).attr("value") == "ac") {
             entry = "";
+        } else if ($(this).attr("value") == "tax") {
+            entry = eval(entry);
+            entry = entry * 1.08;
+            // 切りあげ
+            entry = Math.ceil(entry);
         } else if ($(this).attr("value") != "=") {
             entry += $(this).attr("value");
         } else {
