c015f478cb72790564235c631fb89469aa1c373f
tettori <hajime.naga@gmail.com>
2017-07-22 15:40:49 +0900

    move files in yourpairsname to akinori-sayaya

--- 20170721/akinori-sayaya/app.js
+++ 20170721/akinori-sayaya/app.js
@@ -1,5 +1,39 @@
+var a = "";
+var b = "";
+var c = "";
+var d = "";
 $(function() {
+
+
   $("button").click(function() {
-    $("#display").html($(this).attr("value"));
+	a = a + $(this).attr("value");
+    $(".window").html(a);
   });
+
+  $("#ac").click(function() {
+	a = "";
+    $(".window").html(a);
+  });
+
+  $("#ce").click(function() {
+	a = "CEは現在使われておりません。";
+    $(".window").html(a);
+  });
+
+  $("#plus").click(function() {
+	b = b + a;
+	a = "";
+  });
+
+  $("button").click(function() {
+	$(".temp").html(b);
+  });
+
+  $("#equal").click(function() {
+	b = b + a;
+	c = b + a
+	d = parseFloat(b);
+	$(".window").html(c);
+  });
+
 });
--- 20170721/yourpairsname/app.js
+++ 20170721/yourpairsname/app.js
@@ -1,39 +1,5 @@
-var a = "";
-var b = "";
-var c = "";
-var d = "";
 $(function() {
-	
-	
   $("button").click(function() {
-	a = a + $(this).attr("value");
-    $(".window").html(a);
+    $("#display").html($(this).attr("value"));
   });
-
-  $("#ac").click(function() {
-	a = "";
-    $(".window").html(a);
-  });
-
-  $("#ce").click(function() {
-	a = "CEは現在使われておりません。";
-    $(".window").html(a);
-  });
-
-  $("#plus").click(function() {
-	b = b + a;
-	a = "";
-  });
-	
-  $("button").click(function() {
-	$(".temp").html(b);
-  });
-	
-  $("#equal").click(function() {
-	b = b + a;
-	c = b + a 
-	d = parseFloat(b);
-	$(".window").html(c);
-  });
-
 });
